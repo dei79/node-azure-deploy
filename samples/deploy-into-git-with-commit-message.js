c65ca76c1c7c95684468b3067e17ba0b5707e434
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2017-03-12 14:05:48 +0100

    supporting commit messages and excludes

---
+++ samples/deploy-into-git-with-commit-message.js
@@ -0,0 +1,17 @@
+var uuid = require('node-uuid');
+
+// Load the credentials (create your own file in the format bellow):
+var credentials = require('../.credentials.json')['git01'];
+
+// Get the deployment manager for Azure Blob Storage
+var DeploymentManager= require('../lib/azure-deploy.js').AzureGitDeploymentManager;
+
+// start the deployment (we are deploying the while project folder in the storage)
+var deploymentManager = new DeploymentManager(credentials.url, credentials.user, credentials.secret);
+deploymentManager.deploy('..', ['.git', '.idea', 'node_modules'], 'Demo Commit').then(function() {
+    console.log("DONE");
+    process.exit(0);
+}).catch(function(error) {
+    console.log("ERROR: " + error);
+    process.exit(1);
+});
